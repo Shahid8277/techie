dcddbf07d308b3ff6622aeacd6e816fb54194ede
Shahid Afridi A M <shahidafridiam@Shahids-MacBook-Air.local>
2026-05-14 22:56:39 +0530

    abstraction

--- src/com/fifu/minicourse/core/abstraction/IndiaUPIPay/UPIApp.java
+++ src/com/fifu/minicourse/core/abstraction/IndiaUPIPay/UPIApp.java
@@ -1,10 +1,37 @@
 package com.fifu.minicourse.core.abstraction.IndiaUPIPay;
 
-public abstract class UPI {
+public abstract class UPIApp {
 
-    public void operationDebit(String receiver, String username, double userBalance, double amount) {
-        System.out.println("get the user, check the balance");
+    protected String userName;
+    protected double balance;
+
+    public UPIApp(String userName, double balance) {
+        this.userName = userName;
+        this.balance = balance;
     }
 
-    public abstract void pay();
+    // Common functionality
+    public void checkBalance() {
+        System.out.println(userName + " balance: ₹" + balance);
+    }
+
+    public void sendMoney(String receiver, double amount) {
+
+        if (balance >= amount) {
+            authenticate();
+            balance -= amount;
+
+            System.out.println("₹" + amount +
+                    " sent to " + receiver);
+
+            paymentSuccessMessage();
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    // Abstraction
+    public abstract void authenticate();
+
+    public abstract void paymentSuccessMessage();
 }
--- src/com/fifu/minicourse/core/hashcodeequal/HashCodeAndEqual.java
+++ src/com/fifu/minicourse/core/hashcodeequal/HashCodeAndEqual.java
@@ -1,4 +1,15 @@
 package com.fifu.minicourse.core.hashcodeequal;
 
 public class HashCodeAndEqual {
+
+    public static void main(String[] args) {
+        String s1 = "fifu";
+        String s2 = new String("fifu");
+
+        if (s1.equals(s2)) {
+            System.out.println("equal");
+        } else {
+            System.out.println("not equal");
+        }
+    }
 }
